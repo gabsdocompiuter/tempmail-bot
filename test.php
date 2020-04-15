ffd530a8926d419dc70a8ca2d9c91532bc72ca78
Gabriel Monteiro <gmonteeeiro@gmail.com>
2020-04-15 20:35:19 -0300

    Coloca o simpleHtmlDom dentro do programa principal

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include 'mail.php';
 $tempMail = new TempMail();
 
 // $tempMail->refreshTime();
-// echo $tempMail->getMail();
+echo $tempMail->getMail();
 echo "\n";
 // echo $tempMail->getLastMessage();
 // echo "\n";
